57c28e8e6103aba3e6497c1141a5a2ade75a4fb9
曾彤妤 Riona_tseng <Riona_tseng@bigdome.com.tw>
2021-07-05 15:34:37 +0800

    Initial commit

---
+++ src/main/java/com/tom/Hello.java
@@ -0,0 +1,15 @@
+package com.tom;
+
+import com.sun.org.apache.xerces.internal.dom.PSVIAttrNSImpl;
+import sun.misc.PostVMInitHook;
+
+public class Hello {
+    public static void main(String[] args) {
+//       System.out.println("hello riona");
+       Person p =new Person();
+       p.hello();
+
+
+
+    }
+}
---
+++ src/main/java/com/tom/Person.java
@@ -0,0 +1,10 @@
+package com.tom;
+
+import javax.xml.soap.SOAPPart;
+
+public class Person {
+    public void hello(){
+        System.out.println("hello world");
+    }
+
+}
